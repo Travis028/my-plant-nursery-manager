92997a8c38baab318d35728e98ea6c3eec811d0d
waynetravis <waynetavis123@gmail.com>
2025-08-27 11:27:02 +0300

    initialize changes

--- database.py
+++ database.py
@@ -10,3 +10,9 @@ class Databaseconnction:
     def __enter__(self):
         self.connection = sqlite3.connect(self.host)
         return self.connection
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
+        else:
+            self.connection.commit()
+            self.connection.close()
