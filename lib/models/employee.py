6f1cbb7419cddb4145ce358956df1d35880e14ce
waynetravis <waynetavis123@gmail.com>
2025-08-27 14:21:50 +0300

    commit change made

--- lib/models/employee.py
+++ lib/models/employee.py
@@ -6,3 +6,8 @@ class Employee(Base):
     id = column(Integer, primary_key=True)
     name = column(String, nullable=False)
     role = column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Employee(name={self.name} - {self.role})>"
+    
+    
