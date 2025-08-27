332255fb404681a1b372d8c09ad49b69a9b6cc75
waynetravis <waynetavis123@gmail.com>
2025-08-27 11:25:00 +0300

    initiate changes

--- database.py
+++ database.py
@@ -6,4 +6,7 @@ class Databaseconnction:
     def __init__(self, host):
         self.connection = None
         self.host = host
-        pass
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.host)
+        return self.connection
