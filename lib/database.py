36face211fa9d748b48e021c7544f890730e49fb
waynetravis <waynetavis123@gmail.com>
2025-08-27 19:28:44 +0300

    initialize changes made

--- lib/database.py
+++ lib/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine('sqlite:///nursery.db')
-Session = sessionmaker(bind=engine)
-Session = Session()
+# Base class for models
 Base = declarative_base()
+
+# Database connection
+engine = create_engine("sqlite:///nursery.db", echo=True)
+
+# Session factory
+SessionLocal = sessionmaker(bind=engine)
