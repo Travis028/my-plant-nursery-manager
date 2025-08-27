641e0d1a5db2c9db3ca9d376209feab245082b73
waynetravis <waynetavis123@gmail.com>
2025-08-27 14:13:26 +0300

    commit change made

--- lib/models/customer.py
+++ lib/models/customer.py
@@ -7,4 +7,6 @@ class customer(Base):
     name = column(String, nullable=False)
     email = column(String, nullable=False)
 
+    def __repr__(self):
+        return f"<customer(name={self.name} - {self.email})>"
     
