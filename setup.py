162811c92ae3d05025b21a3f2f422e96412457a5
waynetravis <waynetavis123@gmail.com>
2025-08-27 18:59:21 +0300

    initialize changes made

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from seruptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "sqlalchemy",
+        "alembic",
+        "click",
+
+    ],
+    entry_points={
+        'console_scripts': [
+            "nursery=lib.cli:cli",
+        ],
+    },
+)
