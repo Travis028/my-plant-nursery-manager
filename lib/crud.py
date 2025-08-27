85c265f2343712d7a6441b7984c314f960f5c0ec
waynetravis <waynetavis123@gmail.com>
2025-08-27 22:51:04 +0300

    commit changes made to the file

---
+++ lib/crud.py
@@ -0,0 +1,15 @@
+
+from datatime import data
+from lib.database import SessionLocal
+from lib.models import customer, Employee, Plant, sale
+
+#-----customer CRUD operations-----
+
+def add_customer(first_name, last_name, email):
+    with SessionLocal() as session:
+        customer = customer(first_name=first_name, last_name=last_name, email=email)
+        session.add(customer)
+        session.commit()
+        session.refresh(customer)
+        return customer
+
--- lib/database.py
+++ lib/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Base class for models
 Base = declarative_base()
 
-# Database connection
+
 engine = create_engine("sqlite:///nursery.db", echo=True)
 
-# Session factory
+
 SessionLocal = sessionmaker(bind=engine)
