c4fbdda2df3b29e0be8e196dd2380ec95f8ea9ab
waynetravis <waynetavis123@gmail.com>
2025-08-29 00:38:00 +0300

    commit neww changes

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,3 +10,26 @@ def session():
     session = Session()
     yield session
     session.close()
+
+
+def test_create_plant(session):
+    plant = Plant(name="Orchid", price=15)
+    session.add(plant)
+    session.commit()
+    assert plant.id is not None
+    assert plant.name == "Orchid"
+
+def test_create_customer_and_employee(session):
+    customer = Customer(name="TestUser")
+    employee = Employee(name="Seller")
+    session.add_all([customer, employee])
+    session.commit()
+    assert customer.id is not None
+    assert employee.id is not None
+
+def test_record_sale(session):
+    plant = Plant(name="Cactus", price=7)
+    customer = Customer(name="Buyer")
+    employee = Employee(name="Cashier")
+    session.add_all([plant, customer, employee])
+    session.commit()
