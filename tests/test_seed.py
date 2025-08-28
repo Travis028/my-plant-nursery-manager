aa60b1bf1354a6dd762fb182aabdb55efae8f362
waynetravis <waynetavis123@gmail.com>
2025-08-29 00:40:01 +0300

    commit neww changes

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -9,3 +9,9 @@ def test_seed_creates_data():
     customers = session.query(Customer).all()
     employees = session.query(Employee).all()
     sales = session.query(Sale).all()
+
+
+    assert len(plants) > 0
+    assert len(customers) > 0
+    assert len(employees) > 0
+    assert len(sales) > 0
