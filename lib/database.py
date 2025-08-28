650fb45200bafed953ba708b545a4291ba504062
waynetravis <waynetavis123@gmail.com>
2025-08-29 00:15:39 +0300

    commit changes made

--- lib/database.py
+++ lib/database.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("sqlite:///nursery.db", echo=False, future=True)
-
-Base = declarative_base()
-
+engine = create_engine("sqlite:///nursery.db")
 Session = sessionmaker(bind=engine)
+Base = declarative_base()
