65384ca3fed86aff83dc1e72e6dcb4abc8311e5a
waynetravis <waynetavis123@gmail.com>
2025-08-28 14:29:25 +0300

    commit changes made

--- lib/models.py
+++ lib/models.py
@@ -29,3 +29,13 @@ sale = relationship("sale", back_populates="customer")
 
 def __repr__(self):
     return f"<Customer(name={self.name}, email={self.email}, phone={self.phone})>"
+
+
+class Sale(Base):
+    __tablename__ = "sales"
+
+    id = Column(Integer, primary_key=True)
+    plant_id = Column(Integer, ForeignKey("plants.id"))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    employee_id = Column(Integer, ForeignKey("employees.id"))
+    quantity = Column(Integer, nullable=False)
