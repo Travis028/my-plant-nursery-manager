f74f27e5092e0aa23a1a3cf9ac4c5adcf6b7cfe0
waynetravis <waynetavis123@gmail.com>
2025-08-29 00:24:01 +0300

    commit changes made

---
+++ lib/models/employee.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<Employee(id={self.id}, name='{self.name}')>"
